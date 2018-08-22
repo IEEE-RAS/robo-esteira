c5ec0a5322c95a7556e9546dc35bd2136cde585d
Marcos Vinicius <marcosviniciuscl@outlook.com>
2018-08-22 17:24:54 -0300

    Update Rx.ino

--- Rx.ino
+++ Rx.ino
@@ -4,28 +4,36 @@
 // Download da biblioteca Virtual Wire: http://blog.usinainfo.com.br/wp-content/uploads/2018/02/VirtualWire.zip
 
 #include <VirtualWire.h>          // Inclui biblioteca necessária para comunicação
-#define DATA_PIN 13               
+#define DATA_PIN 13
+               
 
 byte message[VW_MAX_MESSAGE_LEN];    // Variável para armazenagem de mensagens
 
-byte messageLength = VW_MAX_MESSAGE_LEN;    // Tamanho da mensagem
+byte messageLength  = 80;    // Tamanho da mensagem
 
 void setup() {
-
+  pinMode(DATA_PIN, OUTPUT);
   Serial.begin(9600);         // Configura a comunicação serial em 9600
-
+ 
   Serial.println("Dispositivo pronto para receber dados!"); //Imprime na Porta Serial
-
+  
   vw_set_rx_pin(11);         // Configura o pino Digital utilizado pelo Módulo Receptor
   vw_setup(2000);            // Bits por segundo
   vw_rx_start();             // Inicializa o receptor
+ digitalWrite(DATA_PIN, LOW);
 }
 
 void loop() {
+  clearBuffer(message, 80);
+  
   if (vw_get_message(message, &messageLength)) {     // Elimina o bloqueio
-
+    Serial.println(messageLength);
     Serial.print("Recebido: ");      // Imprime na porta serial a mensagem em destaque
-    Serial.write(message[0]);
+    int i;
+    for(i = 0; i < 80; i++){
+      Serial.write(message[i]);  
+    }
+    
 
     if (message[0] == '0') {           // Se receber o valor 0
       digitalWrite(DATA_PIN, LOW);             // Põem o pino 13 em estado LOW
@@ -34,7 +42,14 @@ void loop() {
     else if (message[0] == '1') {      // Se receber o valor 1
       digitalWrite(DATA_PIN, HIGH);            // Põem o pino 13 em estado HIGH
     }
-
+    
     Serial.println();
+  }
+}
+
+void clearBuffer(byte *v, int t){
+  int j;
+  for(j=0; j < t; j++){
+    v[j] = '\0';
   }
 }
