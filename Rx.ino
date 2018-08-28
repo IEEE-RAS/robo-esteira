cc6b79c88df93454bf52ce7e7308f65e1effb8c3
Ananias Correia do Nascimento <ananiascn.fsa@gmail.com>
2018-08-28 15:46:03 -0300

    Update Rx.ino

--- Rx.ino
+++ Rx.ino
@@ -18,7 +18,7 @@ byte messageLength  = 80;    // Tamanho da mensagem
 
 void setup() {
   pinMode(DATA_PIN, OUTPUT);
-  Serial.begin(9600);         // Configura a comunicação serial em 9600
+  Serial.begin(115200 );         // Configura a comunicação serial em 9600
 
   Serial.println("Dispositivo pronto para receber dados!"); //Imprime na Porta Serial
 
@@ -33,43 +33,44 @@ void loop() {
 
   if (vw_get_message(message, &messageLength)) {     // Elimina o bloqueio
     //Serial.println(messageLength);
-    Serial.print("Recebido: ");      // Imprime na porta serial a mensagem em destaque
+    //Serial.print("Recebido: ");      // Imprime na porta serial a mensagem em destaque
     int i;
-    for (i = 0; i < 80; i++) {
-      Serial.write(message[i]);
-    }
+    //for (i = 0; i < 80; i++) {
+      //Serial.write(message[i]);
+    //}
 
     char convertida[messageLength];
     convesorByteChar(message, messageLength, convertida);
     atualizarVetor(convertida, messageLength);
 
-    Serial.print("\nVetor: ");
-    for (i = 0; i < 6; i++) {
-      Serial.print(angulos[i]);
-      Serial.print(" ; ");
-    }
-    Serial.println(movimento);
+    //Serial.print("\nVetor: ");
+    //for (i = 0; i < 6; i++) {
+    //  Serial.print(angulos[i]);
+    //  Serial.print(" ; ");
+    //}
+   // Serial.println(movimento);
+    moverTudo(angulos);
 
   }
 }
 
-int atualizarVetor(char *msg, int t){
-   int i, j=0, k=0;
-   char v[4];
+int atualizarVetor(char *msg, int t) {
+  int i, j = 0, k = 0;
+  char v[4];
 
-   for(i=0; i<t && msg[i]!='\0'; i++){ //percorre o vetor de mensagem
-      if(msg[i]==','){ //verifica se acabou o numero
-         v[k] = '\0'; //delimita o fim da string temporaria
-         angulos[j] = atoi(v); //converte de string para nÃºmero
-         j++; //muda a posicao do vetor angulo
-         k = 0;   //zera a string temporaria
-      }else{ //acomula os digitos do nÃºmero
-         v[k] = msg[i]; 
-         k++;      
-      }   
-   
-   }
-   movimento = msg[i-1];      
+  for (i = 0; i < t && msg[i] != '\0'; i++) { //percorre o vetor de mensagem
+    if (msg[i] == ',') { //verifica se acabou o numero
+      v[k] = '\0'; //delimita o fim da string temporaria
+      angulos[j] = atoi(v); //converte de string para numero
+      j++; //muda a posicao do vetor angulo
+      k = 0;   //zera a string temporaria
+    } else { //acomula os digitos do numero
+      v[k] = msg[i];
+      k++;
+    }
+
+  }
+  movimento = msg[i - 1];
 }
 
 void clearBuffer(byte *v, int t) {
@@ -100,7 +101,7 @@ void moverTudo (int angulos []) {
     Serial.print("#");
     Serial.print(i);
     Serial.print(" P");
-    Serial.print(conversorGraus (angulos[i]), 0);
+    Serial.print(conversorGraus (angulos[i]));
     Serial.print(" ");
   }
   Serial.print("T");
@@ -110,13 +111,12 @@ void moverTudo (int angulos []) {
 
 //Função que converte graus para ms.
 int conversorGraus (int graus) {
-  return 1500 + ((1000 * graus) / 90);
+  return map(graus, -90, 90, 500, 1500);
 }
 
-void convesorByteChar(byte a[], int t, char b[]){
+void convesorByteChar(byte a[], int t, char b[]) {
   int i;
-  for (i=0; i < t; i++){
-    b[i] = (char)a[i]; 
+  for (i = 0; i < t; i++) {
+    b[i] = (char)a[i];
   }
 }
-
