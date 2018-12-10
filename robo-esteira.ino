15ac708bdb684b3b7f65a277fe4ab61df3298506
Marcos Vinicius <marcosviniciuscl@outlook.com>
2018-12-10 07:23:58 -0300

    Adicionado nova biblioteca para comunicação com o novo sensor

--- robo-esteira.ino
+++ robo-esteira.ino
@@ -1,70 +1,86 @@
-#include <Wire.h>
-#include <stdio.h>
+#include "MPU9250.h"
 #include <VirtualWire.h> // Inclui biblioteca necessária para comunicação
+#include <ArduinoJson.h>
 
+// an MPU9250 object with the MPU-9250 sensor on I2C bus 0 with address 0x68
+MPU9250 IMU(Wire,0x68);
+int status;
 
-//------------------- GY --------------------------------
-#define CTRL_REG1 0x20
-#define CTRL_REG2 0x21
-#define CTRL_REG3 0x22
-#define CTRL_REG4 0x23
-#define CTRL_REG5 0x24
+//Usado para calcular o angulo - Variaveis do acelerometro
+double accXangle;
+double accYangle;
+double accZangle;
+//Usado para calcular o angulo - Variaveis do giroscopio
+double gyroXangle = 0;
+double gyroYangle = 0;
+double gyroZangle = 0;
 
-int L3G4200D_Address = 0x69;
-// Can fine-tune this if you need to
-float DPS_MULT = 0.0000085;
+uint32_t timer;
 
-// Delta angles (raw input from gyro)
-int x = 0;
-int y = 0;
-int z = 0;
-// Actual angles
-float angX = 0;
-float angY = 0;
-float angZ = 0;
-// Previous angles for calculation
-float p_angX = 0;
-float p_angY = 0;
-float p_angZ = 0;
-
-// Calibration values
-int gyroLowX = 0;
-int gyroLowY = 0;
-int gyroLowZ = 0;
-int gyroHighX = 0;
-int gyroHighY = 0;
-int gyroHighZ = 0;
-
-// Used for calculating delta time
-unsigned long pastMicros = 0;
-//-------------------------------------------------------
-
+typedef struct {
+  double gyroXangle;
+  double gyroYangle;
+  double gyroZangle; 
+  
+} Values;
 
 
 void setup() {
-  Serial.begin(9600);
-  Wire.begin();
+// serial to display data
+  Serial.begin(115200);
+  while(!Serial) {}
+  Serial.println("Serial started");
+
   vw_set_tx_pin(11);       // Configura o pino Digital utilizado pelo Módulo Transmissor
   vw_setup(2000);          // Bits per segundo
-  //configurarGyr();
+
+  // start communication with IMU 
+  status = IMU.begin();
+  status = IMU.setGyroRange(MPU9250::GYRO_RANGE_1000DPS);
+  if (status < 0) {
+    Serial.println("IMU initialization unsuccessful");
+    Serial.println("Check IMU wiring or try cycling power");
+    Serial.print("Status: ");
+    Serial.println(status);
+    while(1) {}
+  }
+  timer = micros();
 }
 
 
-
 void loop() {
-  char data[80];
-  /*getGyroValues();
-  calculo();
-  sprintf(data, "X: %d Y: %d Z: %d", x, y, z);
-  send(data);                    // Envia o valor para o receptor
-  delay(100);
-  */
-  int i;
-  for(i=-90; i < 90; i++){
-    sprintf(data, "%d,%d,%d,%d,%d,%d,D", i, i, i, i, i, i);
-    send(data);                    // Envia o valor para o receptor
-    delay(100);
-  }
+
+  //Read the sensor
+  IMU.readSensor();
+  Values r = getGyro();
+  sendJson(r.gyroXangle, r.gyroYangle, r.gyroZangle, 0);
+}
+
+Values getGyro(){
+  int16_t ax = IMU.getAccelX_mss(), ay = IMU.getAccelY_mss(), az = IMU.getAccelZ_mss();
+  int16_t gx = IMU.getGyroX_rads(), gy = IMU.getGyroY_rads(), gz = IMU.getGyroZ_rads();
+  
+  // Calcular os angulos com base nos sensores do acelerometro
+  accXangle = (atan2(ax,az) + PI) * RAD_TO_DEG;
+  accYangle = (atan2(ay,az) + PI) * RAD_TO_DEG;
+  accZangle = (atan2(ax,ay) + PI) * RAD_TO_DEG;  
+
+  double gyroXrate = ((double)gx / 131.0);
+  double gyroYrate = -((double)gy / 131.0);
+  double gyroZrate = -((double)gz / 131.0);
+  
+  //###################### Calcular o ângulo de giro sem qualquer filtro ######################### 
+  gyroXangle += gyroXrate*((double)(micros()-timer)/100); 
+  gyroYangle += gyroYrate*((double)(micros()-timer)/100);
+  gyroZangle += gyroZrate*((double)(micros()-timer)/100);
+
+  timer = micros();
+  Values r;
+  r.gyroXangle = gyroXangle;
+  r.gyroYangle = gyroYangle;
+  r.gyroZangle = gyroZangle;
+//  delay(1);
+  return r;
 }
 
 void send (char *message) {
@@ -73,150 +89,20 @@ void send (char *message) {
   vw_wait_tx();                  // Aguarda o envio de dados
 }
 
-void calculo(){
-  // Calculate delta time
-  float dt;
-  if(micros() > pastMicros) //micros() overflows every ~70 minutes
-    dt = (float) (micros()-pastMicros)/1000000.0;
-  else
-    dt = (float) ((4294967295-pastMicros)+micros())/1000000.0;
-
-
-  // Calculate angles
-  if(x >= gyroHighX || x <= gyroLowX) {
-    angX += ((p_angX + (x * DPS_MULT))/2) * dt;
-    p_angX = x * DPS_MULT;
-  } else {
-    p_angX = 0;
-  }
-
-  if(y >= gyroHighY || y <= gyroLowY) {
-    angY += ((p_angY + (y * DPS_MULT))/2) * dt;
-    p_angY = y * DPS_MULT;
-  } else {
-    p_angY = 0;
-  }
-
-  if(z >= gyroHighZ || z <= gyroLowZ) {
-    angZ += ((p_angZ + (z * DPS_MULT))/2) * dt;
-    p_angZ = z * DPS_MULT;
-  } else {
-    p_angZ = 0;
-  }
-
-  pastMicros = micros();
-
-  sendJson();
-  
-  delay(10);
-}
-
-void configurarGyr(){
-  Wire.begin();
-  setupL3G4200D(250);           // Configure L3G4200  - 250, 500 or 2000 deg/sec
-  delay(1000);                  // wait for the sensor to be ready 
-  calibrate();
-  attachInterrupt(0, updateAngle, RISING);
-  pastMicros = micros();
-}
-
-void updateAngle(){
-  getGyroValues();
-}
-
-// Print angles to Serial (for use in Processing, for example)
-void sendJson() {
-    char json[40];
-    sprintf(json, "{\"x\":%d,\"y\":%d,\"z\":%d}", (int)(angX*1000), (int)(angY*1000), (int)(angZ*1000));
-    Serial.println(json);
-}
-
-void getGyroValues() {
-  byte xMSB = readRegister(L3G4200D_Address, 0x29);
-  byte xLSB = readRegister(L3G4200D_Address, 0x28);
-  x = ((xMSB << 8) | xLSB);
-
-  byte yMSB = readRegister(L3G4200D_Address, 0x2B);
-  byte yLSB = readRegister(L3G4200D_Address, 0x2A);
-  y = ((yMSB << 8) | yLSB);
-
-  byte zMSB = readRegister(L3G4200D_Address, 0x2D);
-  byte zLSB = readRegister(L3G4200D_Address, 0x2C);
-  z = ((zMSB << 8) | zLSB);
-}
-
-int setupL3G4200D(int scale) {
-  //From  Jim Lindblom of Sparkfun's code
-
-  // Enable x, y, z and turn off power down:
-  writeRegister(L3G4200D_Address, CTRL_REG1, 0b00001111);
-
-  // If you'd like to adjust/use the HPF, you can edit the line below to configure CTRL_REG2:
-  writeRegister(L3G4200D_Address, CTRL_REG2, 0b00000000);
-
-  // Configure CTRL_REG3 to generate data ready interrupt on INT2
-  // No interrupts used on INT1, if you'd like to configure INT1
-  // or INT2 otherwise, consult the datasheet:
-  writeRegister(L3G4200D_Address, CTRL_REG3, 0b00001000);
-
-  // CTRL_REG4 controls the full-scale range, among other things:
-
-  if(scale == 250) {
-    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00000000);
-  } else if(scale == 500) {
-    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00010000);
-  } else {
-    writeRegister(L3G4200D_Address, CTRL_REG4, 0b00110000);
-  }
-
-  // CTRL_REG5 controls high-pass filtering of outputs, use it
-  // if you'd like:
-  writeRegister(L3G4200D_Address, CTRL_REG5, 0b00000000);
-}
-
-void writeRegister(int deviceAddress, byte address, byte val){
-    Wire.beginTransmission(deviceAddress); // start transmission to device 
-    Wire.write(address);       // send register address
-    Wire.write(val);         // send value to write
-    Wire.endTransmission();     // end transmission
-}
-
-int readRegister(int deviceAddress, byte address){
-    int v;
-    Wire.beginTransmission(deviceAddress);
-    Wire.write(address); // register to read
-    Wire.endTransmission();
-
-    Wire.requestFrom(deviceAddress, 1); // read a byte
-
-    while(!Wire.available()) {
-        // waiting
-    }
-    v = Wire.read();
-    return v;
-}
-
-void calibrate(){
-  Serial.println("Calibrando giroscopio, não meixa!");
-  for(int i = 0; i < 200; i++) {
-    getGyroValues();
-
-    if(x > gyroHighX)
-      gyroHighX = x;
-    else if(x < gyroLowX)
-      gyroLowX = x;
-
-    if(y > gyroHighY)
-      gyroHighY = y;
-    else if(y < gyroLowY)
-      gyroLowY = y;
-
-    if(z > gyroHighZ)
-      gyroHighZ = z;
-    else if(z < gyroLowZ)
-      gyroLowZ = z;
+void sendJson(double x, double y, double z, double p) {
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject& root = jsonBuffer.createObject();
+    root["x"] = (int)x;
+    root["y"] = (int)y;
+    root["z"] = (int)z;
+    root["p"] = p;
+    root["time"] = micros();
     
-    delay(10);
-  }
-  Serial.println("Calibrado.");
+    char json[100];
+
+    root.printTo(json);
+
+//    sprintf(json, "{\"x\":%f,\"y\":%f,\"z\":%f, \"p\":%f, \"timer\":%d}", f, y, z, p, micros());
+    Serial.println(json);
+//    send(json);
 }
